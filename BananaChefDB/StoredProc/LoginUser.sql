e70bbf8d4bafc2f87ff2e92e8346b7b4fd64b2a3
BuildTools <unconfigured@null.spigotmc.org>
2023-06-19 15:13:30 +0200

    Ajoutez des fichiers projet.

---
+++ BananaChefDB/StoredProc/LoginUser.sql
@@ -0,0 +1,63 @@
+﻿CREATE PROCEDURE [dbo].[LoginUser]
+	@identifier NVARCHAR(255),
+	@password NVARCHAR(100)
+AS
+BEGIN
+	DECLARE @userId UNIQUEIDENTIFIER
+	DECLARE @storedPassword VARBINARY(64)
+	DECLARE @storedSalt VARCHAR(100)
+
+	/*
+	* Identification email ou pseudo
+	*/
+	IF CHARINDEX('@', @identifier) > 0
+	-- si email
+		BEGIN
+			SELECT @userId = UserID, @storedPassword = Pwd, @storedSalt = Salt
+			FROM [dbo].[User]
+			WHERE Email = @identifier
+		END
+	ELSE
+	-- si pseudo
+		BEGIN
+			SELECT @userId = UserID, @storedPassword = Pwd, @storedSalt = Salt
+			FROM [dbo].[User]
+			WHERE Username = @identifier
+		END
+
+	/*
+	* Control userId
+	*/
+	IF @userId IS NOT NULL
+		BEGIN
+			/*
+			* HashKey
+			* Recup secret key
+			*/
+			DECLARE @hashKey VARCHAR(100)
+			SET @hashKey = dbo.GetSecret()
+
+			/*
+			* inputPassword
+			* hashage du inputPassword en SHA2_512 avec le @storedSalt, @hashKey, @password, @storedSalt
+			*/
+			DECLARE @inputPassword VARBINARY(64)
+			SET @inputPassword = HASHBYTES('SHA2_512', CONCAT(@storedSalt,@hashKey,@password, @storedSalt))
+
+			/*
+			* Control pwd
+			*/
+			IF @inputPassword = @storedPassword
+				BEGIN
+					SELECT 'Login successful' AS Message, @userId AS UserID
+				END
+			ELSE
+				BEGIN
+					SELECT 'Incorrect Username/Email or password' AS Message, NULL AS UserID
+				END
+		END
+	ELSE
+		BEGIN
+			SELECT 'Incorrect Username/Email or password' AS Message, NULL AS UserID
+		END
+END
---
+++ BananaChefDB/StoredProc/RegisterUser.sql
@@ -0,0 +1,51 @@
+﻿CREATE PROCEDURE [dbo].[RegisterUser]
+	@username NVARCHAR(30),
+	@email NVARCHAR(255),
+	@pwd NVARCHAR(100)
+AS
+BEGIN
+	/*
+	* Salt
+	* UUID + HASH ( email + username ) + UUID
+	*/
+	DECLARE @salt VARCHAR(100)
+	SET @salt = CONCAT(NEWID(), HASHBYTES('SHA2_512',CONCAT(@email, @username)), NEWID())
+
+	/*
+	* HashKey
+	* Recup secret key
+	*/
+	DECLARE @hashKey VARCHAR(320) 
+	SET @hashKey = dbo.GetSecret()
+
+	/*
+	* pwdHash
+	* hashage du pwd en SHA2_512 avec le salt, hashkey, pwd, salt
+	*/
+	DECLARE @pwdHash VARBINARY(64)
+	SET @pwdHash = HASHBYTES('SHA2_512', CONCAT(@salt, @hashKey, @pwd, @salt))
+
+	/*
+	* userId
+	* generation l'id user en UUID
+	*/
+	DECLARE @userId UNIQUEIDENTIFIER
+	SET @userId = NEWID()
+
+	/*
+	* Control si l'email et le pseudo existe
+	* si ok = insert
+	* si ko = erreur
+	*/
+	IF NOT EXISTS (SELECT 1 FROM [dbo].[User] WHERE Username = @username)
+		AND NOT EXISTS (SELECT 1 FROM [dbo].[User] WHERE Email = @email)
+		BEGIN
+			INSERT INTO [dbo].[User] (UserID, Username, Email, Pwd, Salt)
+			VALUES (@userId,@username,@email,@pwdHash,@salt)
+		END
+	ELSE
+		BEGIN
+			RAISERROR('Username or email already exists.', 16, 1)
+			RETURN
+		END
+END
